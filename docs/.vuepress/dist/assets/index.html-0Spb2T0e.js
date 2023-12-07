74a7c455df985760c5e24edbcf107361f4006f26
oseer <oseer.williams@teradata.com>
2023-12-07 14:35:27 -0500

    more updates

--- docs/.vuepress/dist/assets/index.html-0Spb2T0e.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as a,c as n,a as e,b as r}from"./app-sfKQ86xV.js";const s={},c=e("h1",{id:"getting-started",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#getting-started","aria-hidden":"true"},"#"),r(" Getting started")],-1),d=[c];function o(i,_){return a(),n("div",null,d)}const l=t(s,[["render",o],["__file","index.html.vue"]]);export{l as default};
--- docs/.vuepress/dist/assets/index.html-WCHBdKR_.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-ff762602","path":"/guides/","title":"Getting started","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1701969411000,"contributors":[{"name":"oseer","email":"oseer.williams@teradata.com","commits":1}]},"filePathRelative":"guides/README.md"}');export{e as data};
